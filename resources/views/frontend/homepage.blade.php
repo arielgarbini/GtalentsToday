ce209cb659a415662259976cffbb3736d439731d
arielgarbini <ariel.garbini@gmail.com>
2017-04-27 20:42:37 +0200

    Mapa landing

--- resources/views/frontend/homepage.blade.php
+++ resources/views/frontend/homepage.blade.php
@@ -242,12 +242,9 @@
 	</article>
 
 	<!--CONTENEDOR MAPA-->
-	<article class="contact">
+	<article class="titleWin contact bloque">
 		<!--MAPA-->
-		<section class="mapa">
-			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d31389.182690366797!2d-64.15356708862302!3d10.449494569045312!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sve!4v1478370590321" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
-		</section>
-
+		
 		<!--FORMULARIO DE CONTACTO-->			
 		<section class="contact-formulario">
 			<!--TITULO
@@ -995,4 +992,5 @@
 	    	}
 	    );
 	</script>
+ript>
 @stop	
